9ca7e59afa7064803ec55f2e61fbab19e2748f79
yousefghanem <yousef765765gh@gmail.com>
2026-01-17 19:01:04 +0300

    footer and share componant

---
+++ src/components/Footer/IconButton.jsx
@@ -0,0 +1,16 @@
+import { NavLink} from "react-router-dom"
+import "./IconButton.css"
+const IconButton = ({items}) => {
+    return (
+        <div className="y-Icon-button">
+            <p>Stay Connected</p>
+            <ul>
+                {items.map((item,index) =>(
+                        <div className="icon" key={index}><li ><NavLink href={item.Social_Media}><img src={item.image}  alt="" /></NavLink></li></div>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default IconButton;
