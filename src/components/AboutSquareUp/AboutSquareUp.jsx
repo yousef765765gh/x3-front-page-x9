6dd08a15a52b2f917523064bd2fe88062c04070e
YousefBrro <ybrro2003@gmail.com>
2026-01-21 14:45:43 +0300

    AboutSquareUp

---
+++ src/components/AboutSquareUp/AboutSquareUp.jsx
@@ -0,0 +1,38 @@
+import "./AboutSquareUp.css"
+import AboutHeroSection from '../about_hero_section/AboutHeroSection';
+import heroabout from "/assets/img/about us section Home+hero work.png"
+
+
+const AboutSquareUp = ({title , description , bgImage , icon}) => {
+    return (
+        <div className="space-x">
+            <AboutHeroSection
+                hasButton={false}            
+                imageBackground={heroabout}
+                titleSection="About Us"
+                contentSection="Welcome to SquareUp, where collaboration, expertise, and client-centricity intersect to shape the future of digital innovation."
+            />
+            <div className="yb-about-squareUp">
+                <div className="yb-squareUp-text">
+                    <h2 className="yb-squareUp-title">{title}</h2>
+                    <p className="yb-squareUp-description">{description}</p>
+                </div>
+                <div className="yb-about-image"
+                style={{backgroundImage:`url(${bgImage})`}}
+                ><div className="yb-lines">
+                <div className="yb_TopheroBackground"></div>
+                        <div className="yb-line yb-line1"></div>
+                        <div className="ybline yb-line2"></div>
+                        <div className="yb-line yb-line3"></div>
+                        <div className="yb-line yb-line4"></div>
+                    </div>
+                    <div className="yb-icon-wrapper">
+                        <img className="yb-icon-squareUp" src={icon}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutSquareUp
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,18 @@
+import AboutSquareUp from "../components/AboutSquareUp/AboutSquareUp"
 import SectionOurStory from "../components/SectionOurStory/SectionOurStory"
+import AboutSquareUpImage from "/assets/img/SquareUpsectionabout.png"
+import SquareUpImage from "/assets/img/LogoSq.svg"
 
 const About = () => {
+
     return (
         <>
+        <AboutSquareUp
+        title="About SquareUp"
+        description="SquareUp is a digital product agency that is passionate about crafting exceptional digital experiences. We specialize in design, engineering, and project management, helping businesses thrive in the digital landscape. At SquareUp, we follow a structured and collaborative process to ensure the successful delivery of exceptional digital products. Our process combines industry best practices, creative thinking, and a client-centric approach."
+        bgImage={AboutSquareUpImage}
+        icon={SquareUpImage}
+        />
             <SectionOurStory/>
         </>
     )
