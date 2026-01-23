1a9f7b7b63718bdf943fde452366538671dacd4c
yousefghanem <yousef765765gh@gmail.com>
2026-01-23 23:19:02 +0300

    x3-fx

--- src/components/DASlider/DASlider.jsx
+++ src/components/DASlider/DASlider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef  } from "react";
 import "./DASlider.css";
 import CardsData from "../../data/TestimonialsCards.json";
 import SliderCardHome from "../Slidercardhome/SliderCardHome";
