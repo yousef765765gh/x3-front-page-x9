90d6d0a4967935ec4cdd54ad40bc695195c8f34e
loujain-alsaleh <loujainalsaleh1@gmail.com>
2026-01-26 23:47:41 +0200

    edit work

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,8 +1,21 @@
+import sqicon from "/assets/img/LogoSq.svg"
+import workpic from "/assets/img/about us section Home+hero work.png"
+import WorksSection from "../components/WorksSection/WorksSection"
+import AboutHeroSection from "../components/about_hero_section/AboutHeroSection"
+
 const Work = () => {
     return (
-        <div>
-            <h2>hello work</h2>
-        </div>
+        <>
+            <WorksSection />
+            <AboutHeroSection
+                hasButton={true}
+                imageBackground={workpic}
+                imageIcone={sqicon}
+                titleSection="Our Works"
+                contentSection="Discover a portfolio of visually stunning and strategically crafted digital projects that showcase our creativity and expertise."
+                buttonText="Start Project"
+            />
+        </>
     )
 }
 
